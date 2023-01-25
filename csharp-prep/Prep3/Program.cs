b727d728d0d766ee994e6f99ec44604ac24411a3
sucre2020 <ettasucre@gmail.com>
2023-01-25 06:56:59 +0100

    GuessGame

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int numb = 109;
+        int g = 110; 
+        
+            Console.Write($"Welcome\nEnter a magic number between 1 - {g}: ");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n != numb)
+            {
+            Console.Write($"Enter a number between 1 - {g}: ");
+            n = int.Parse(Console.ReadLine());
+            
+            if (n > numb)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (n < numb)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.Write($"Congraluations you guess {numb} right");
+            }
+            }
+
+        }
     }
-}
