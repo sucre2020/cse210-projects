3f60c736802eaaedd87de4c19e97100d43585ddb
sucre2020 <ettasucre@gmail.com>
2023-03-19 05:18:23 -0600

    Test

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        int elements = 3;
+        string[] words = new string [elements];
+        words[0] = "Have";
+        words[1] = "Some";
+        words[2] = "Coffee";
+        string WORD = words[2];
+        string WORD1 = words[1];
+        string WORD0 = words[0];
+        Console.WriteLine($"Hello Learning03 World!, I'm learning about so many things including {WORD}, even though I don't take it\n");
+        Console.WriteLine($"Hello Learning03 World! do you care to {WORD0} {WORD1} {WORD}?\n");
+        Console.WriteLine("This is a test!");        
+
     }
+
+
+    
 }
