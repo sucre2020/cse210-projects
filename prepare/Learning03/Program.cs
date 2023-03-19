1911362e39f685e883db541faa37db6dfe2f56b3
sucre2020 <ettasucre@gmail.com>
2023-03-19 06:04:37 -0600

    fractional

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,17 +5,17 @@ class Program
     static void Main(string[] args)
     {
 
-        Person Me = new Person();
-        Me.firstName = "Efita";
-        Me.age = "Ask Google";
-        Me.lastName = "Effiom";
-        Me.country = "Celestial Kingdom";
+       // Person Me = new Person();
+       // Me.firstName = "Efita";
+       // Me.age = "Ask Google";
+       // Me.lastName = "Effiom";
+        //Me.country = "Celestial Kingdom";
 
 
-        Console.WriteLine(Me.firstName);
-        Console.WriteLine(Me.lastName);
-        Console.WriteLine(Me.age);
-        Console.WriteLine(Me.country);
+        //Console.WriteLine(Me.firstName);
+        //Console.WriteLine(Me.lastName);
+        //Console.WriteLine(Me.age);
+        //Console.WriteLine(Me.country);
 
         //int elements = 3;
         //string[] words = new string [elements];
@@ -29,18 +29,31 @@ class Program
         //Console.WriteLine($"Hello Learning03 World! do you care to {WORD0} {WORD1} {WORD}?\n");
        // Console.WriteLine("This is a test!");      
 
+    //}
+
+   // class Person
+   // {
+       // public string firstName;
+       // public string lastName;
+       // public string age;
+       // public string country;
+       // private string politicalAffiliation;
+    //}
+
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
-
-    class Person
-    {
-        public string firstName;
-        public string lastName;
-        public string age;
-        public string country;
-        private string politicalAffiliation;
-    }
-
-
-
-    
 }
